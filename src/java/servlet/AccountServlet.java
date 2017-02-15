6498adaae20d6c1c7ec455a73c6776540e4de47b
nisarg242 <nessrana242@gmail.com>
2017-02-15 12:09:16 -0500

    Changes 3

--- src/java/servlet/AccountServlet.java
+++ src/java/servlet/AccountServlet.java
@@ -31,31 +31,35 @@ public class AccountServlet extends HttpServlet {
     
     Account accnt = new Account();
     
-    protected void doGet(HttpServletResponse response, HttpServletRequest request) throws IOException
-    {
-        response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
-        response.setHeader("Pragma", "no-cache");
-        response.setDateHeader("Expires", 0);
-        response.getWriter().write(String.valueOf(accnt.getBalance()));
-    }
     
-    protected void doPost(HttpServletResponse response, HttpServletRequest request) throws IOException
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
-        if(request.getParameter("withdraw")!= null)
+        if(request.getParameter("deposit")!= null)
         {
-            double withdraw = Double.parseDouble(request.getParameter("withdraw"));
-            accnt.withdraw(withdraw);
+            double deposit1 = Double.parseDouble(request.getParameter("deposit"));
+            accnt.deposit(deposit1);
         }
-        else if(request.getParameter("deposit")!= null)
+        else if(request.getParameter("withdraw")!= null)
         {
-            double deposit = Double.parseDouble(request.getParameter("deposit"));
-            accnt.deposit(deposit);
+           
+             double withdraw1 = Double.parseDouble(request.getParameter("withdraw"));
+            accnt.withdraw(withdraw1);
         }
         else
         {
             accnt.close();
         }
         
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.getWriter().write(String.valueOf(accnt.getBalance()));
     }
     
 }
